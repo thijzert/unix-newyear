025aa08a93d1eeaccc9dd7c55b8680c25c1c82f9
Thijs van Dijk <thijs@hooi.gr>
2015-11-11 20:18:46 +0100

    Nice animated count-up clock

--- assets/script/main.go
+++ assets/script/main.go
@@ -21,8 +21,27 @@ func incrementClock() {
 	ct := fmt.Sprintf("%9x", time.Now().Unix())
 
 	jq("#clock .digit").Call("each", func(i int, x *js.Object) {
+		// i'th digit
 		dg := ct[i : i+1]
 
-		jq(x).SetText(dg).Call("toggleClass", "empty", dg == " ")
+		// Digit currently displayed
+		cur_dig := jq("div.current", x)
+		cd := cur_dig.Text()
+
+		if cd != dg {
+			new_dig := jq(".next", x).SetText(dg)
+
+			new_dig.Call("toggleClass", "current", true).Call("toggleClass", "next", false)
+			cur_dig.Call("toggleClass", "prev", true).Call("toggleClass", "current", false)
+
+			js.Global.Get("setTimeout").Invoke(func() {
+				jq(".prev", x).Call("remove")
+				jq(x).Call("append", jq("<div class=\"next\"></div>"))
+			}, 500)
+		}
+		if i == 0 && dg != " " {
+			// Happy new Epoch!
+			jq(x).Call("toggleClass", "empty", false)
+		}
 	})
 }
